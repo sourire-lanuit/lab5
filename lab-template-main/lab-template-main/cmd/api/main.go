57b05caff83d4c720e1b8ffada4f296a08bc43f9
sophie-lanuit <sofigaran-ipt28@lll.kpi.ua>
2025-06-05 13:12:54 +0300

    updated files

--- lab-template-main/lab-template-main/cmd/api/main.go
+++ lab-template-main/lab-template-main/cmd/api/main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-
-	"github.com/jackc/pgx/v5/pgxpool"
-	db "github.com/nochzato/example/db/sqlc"
-	"github.com/nochzato/example/internal/server"
+	"log"
+	"net/http"
+	"github.com/sourire-lanuit/lab5/handlers"
 )
 
 func main() {
-	connPool, err := pgxpool.New(context.Background(), "postgresql://postgres:postgres@localhost:5432/api_db")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer connPool.Close()
-
-	store := db.NewStore(connPool)
-
-	server := server.NewServer(store)
-	server.Run(":3000")
+	http.HandleFunc("/books", handlers.CreateBook) // тільки POST поки
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
